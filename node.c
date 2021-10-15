66be7e69614f2ae1fd2fad1f5d1cd9022604e5d8
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-10-15 16:39:40 +0300

    Add DFS traversal

--- node.c
+++ node.c
@@ -1,4 +1,19 @@
 #include "node.h"
+#include <stdio.h>
+
+static void traverse_r(const NODE* const node, CONST_TREE_ARRAY array, int * index){
+	if(node == NULL)
+		return;
+
+	if(node->character != '\0'){
+		printf("wrote char %c in index:%d\n", node->character, *(index));
+		array[*(index)] = node;
+		*(index) = *(index) + 1;
+	}
+
+	traverse_r(node->left, array, index);
+	traverse_r(node->right, array, index);
+}
 
 NODE* new_node(){
 	return (NODE*) calloc(1, sizeof(NODE)); //set all to NULL/0
@@ -7,4 +22,9 @@ NODE* new_node(){
 void destroy_node(NODE* node){
 	free(node);
 	node = 0;
+}
+
+void traverse(const NODE* const root, CONST_TREE_ARRAY array){
+	static int index = 0;
+	traverse_r(root, array, &index);
 }
